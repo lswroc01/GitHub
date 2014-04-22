4299fb43e8fef74784544f236df994bc93e82a77
a0mart17 <a0mart17@cardmail.louisville.edu>
2014-04-22 06:40:53 -0700

    modified Identity model and Register view

--- CIS411-FinalProject/GamesVille/GamesVille/Controllers/GamesController.cs
+++ CIS411-FinalProject/GamesVille/GamesVille/Controllers/GamesController.cs
@@ -46,7 +46,7 @@ namespace GamesVille.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="ID,Title,ReleaseDate,Platform,ESBR_Rating,Category,Rating")] Game game)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Platform,ESBR_Rating,Category,Rating,PictureURL")] Game game)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace GamesVille.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="ID,Title,ReleaseDate,Platform,ESBR_Rating,Category,Rating")] Game game)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Platform,ESBR_Rating,Category,Rating,PictureURL")] Game game)
         {
             if (ModelState.IsValid)
             {
